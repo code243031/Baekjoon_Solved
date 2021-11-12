4768e9896f3ade0543fa2612be9dac2ede770e3a
J <37607965+code243031@users.noreply.github.com>
2021-11-12 09:45:51 +0900

    2021-11-12 update
    solved list
    - 11441 : 합 구하기

---
+++ Silver3/11441.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static System.Math;
+
+namespace ConsoleApplication26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new(),
+                      add = new();
+
+            int N = int.Parse(Console.ReadLine());
+
+            string[] buf = Console.ReadLine().Split(" ");
+            int res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                arr.Add(int.Parse(buf[i]));
+                res += int.Parse(buf[i]);
+                add.Add(res);
+            }
+
+            N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                buf = Console.ReadLine().Split(" ");
+                res = add[int.Parse(buf[1]) - 1]; // - add[int.Parse(buf[0]) - 1];
+
+                if (int.Parse(buf[0]) != 1)
+                {
+                    res -= add[int.Parse(buf[0]) - 2];
+                }
+
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
